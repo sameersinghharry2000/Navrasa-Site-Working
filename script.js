0cfdad62b1bff2f5bb4fd0695f71b4ac6b1caf25
Sameer Singh <136222372+sameersinghharry2000@users.noreply.github.com>
2024-05-18 02:09:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 let navimg = document.querySelector(".logo");
 let nav = document.querySelector(".nav");
-let navUl = document.querySelector(".navUl");
+// let navUl = document.querySelectorAll(".navUl");
+
 
 window.addEventListener("scroll",()=> {
     if(window.scrollY >= 50 ){
         nav.style.backgroundColor="white";
         navimg.style.filter="invert(0%)"
-        navUl.style.color="black";
+        // navUl.style.color="black";
+        menubtn.style.filter="invert(0)"
     }
     else{
         nav.style.backgroundColor="transparent";
         navimg.style.filter="invert(100%)"
-        navUl.style.color="white";
+        // navUl.style.color="white";
+        menubtn.style.filter="invert(200%)"
     }
 })
 
-let cont1leftbtn = document.querySelector(".cont1leftbtn");
-let cont1rightbtn = document.querySelector(".cont1rightbtn");
-let slide1 = document.querySelector(".slide1");
-let slide2 = document.querySelector(".slide2");
-let slide3 = document.querySelector(".slide3");
-let slide4 = document.querySelector(".slide4");
+let menubtn = document.getElementById("menubtn");
+let slidebar = document.querySelector(".slidebar");
+
+    menubtn.addEventListener("click",()=>{
+        slidebar.style.right="0%";
+    })
+    slidebar.addEventListener("click",()=>{
+        slidebar.style.right="-100%"
+    })
+
+
+
+
+
+
+
+// let cont1leftbtn = document.querySelector(".cont1leftbtn");
+// let cont1rightbtn = document.querySelector(".cont1rightbtn");
+// let slide1 = document.querySelector(".slide1");
+// let slide2 = document.querySelector(".slide2");
+// let slide3 = document.querySelector(".slide3");
+// let slide4 = document.querySelector(".slide4");
 
